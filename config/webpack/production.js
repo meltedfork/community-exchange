7973bc04c5f5762b7aa5555857d8ca4fcda201f4
Suzanne Salerno <suzsalerno2@gmail.com>
2025-01-01 23:39:18 -0600

    babel core

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,6 +2,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
 const environment = require('./environment');
 
-environment.config.set(outputPath.hashFunction, 'xxhash64');
+environment.config.set('output.hashFunction', 'xxhash64');
 
 module.exports = environment.toWebpackConfig();
