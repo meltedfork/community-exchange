3fc7cc2a4a7921c8c34be532f65ad9eb4c6482aa
Suzanne Salerno <suzsalerno2@gmail.com>
2025-01-01 23:50:33 -0600

    xxhash-addon

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,6 +2,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
 const environment = require('./environment');
 
-environment.config.set('output.hashFunction', 'xxhash64');
+environment.config.set('output.hashFunction', require('xxhash-addon').XXHash64);
 
 module.exports = environment.toWebpackConfig();
