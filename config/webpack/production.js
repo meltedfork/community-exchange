2c5b1fed689f1b6c2e7a021e47d78ee0b541e723
Jason Salerno <jason@meltedspork.com>
2025-01-07 22:23:57 -0600

    test

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,6 +2,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
 const environment = require('./environment');
 
-environment.config.set('output.hashFunction', require('hash-wasm').md4);
+//environment.config.set('output.hashFunction', require('hash-wasm').md4);
 
 module.exports = environment.toWebpackConfig();
