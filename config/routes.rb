9cb87ccd873de34a371331f8a11ade2517edfd57
Suzanne Salerno <suzsalerno2@gmail.com>
2025-04-28 22:51:29 -0500

    Adds mailchimp html, modified styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'pages/about'
   get 'pages/landing'
+  get 'pages/about'
+  get 'pages/signup'
   root 'pages#landing'
 end
